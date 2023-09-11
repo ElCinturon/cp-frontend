b87ebbe9b6714202fcda341f35010da085bcfd6e
Michel Bitter <michelbitter@live.de>
2023-09-11 13:25:45 +0200

    Homeseite für Eingeloggte User erstellen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { CookieService } from "ngx-cookie-service";
-import { UserInfoService } from 'src/app/services/userInfo.service';
+import { UserInfoService } from "src/app/services/userInfo.service";
+import { PortfolioService } from "../services/portfolio.service";
+import { Portfolio } from "../shared/interfaces/Portfolio";
 
 @Component({
   selector: "app-home",
@@ -9,7 +11,10 @@ import { UserInfoService } from 'src/app/services/userInfo.service';
 })
 export class HomeComponent {
   username!: string;
-  constructor(private cookieService: CookieService, private userInfoService: UserInfoService) { }
+  amountPortfolios = 0;
+  mostValuablePortfolio: Portfolio | null = null;
+
+  constructor(private cookieService: CookieService, private userInfoService: UserInfoService, private portfolioService: PortfolioService) { }
 
   // TODO Code doppelt mit appmodule. Möglichkeit für globale Userinfos schaffen
   ngOnInit() {
@@ -17,7 +22,6 @@ export class HomeComponent {
     // Zunächst Usernamen aus Cookie auslesen (Für Reload wichtig)
     if (!this.username) {
       this.username = this.cookieService.get("username");
-      console.log(this.username);
     }
 
 
@@ -28,5 +32,29 @@ export class HomeComponent {
       },
       error: (error) => { console.error("error bei Abruf von Userinfos:", error); }
     });
+
+    if (this.username) {
+      this.getPortfolioStats();
+    }
   }
+
+  // Ruft anhand aller Portfolios Statistiken für den User ab
+  getPortfolioStats() {
+    this.portfolioService.getPortfolios().subscribe({
+      next: (response) => {
+        if (response.length > 0) {
+          this.amountPortfolios = response.length;
+          this.mostValuablePortfolio = response.reduce((prev, current) => prev.totalValue! > current.totalValue! ? prev : current);
+        } else {
+          this.amountPortfolios = 0;
+        }
+      },
+      error: (error) => { console.log("Beim Abruf der Portfoliostatistiken ist ein Fehler aufgetreten", error) },
+    })
+  }
+
+
+
+
+
 }
