4d6ae0384791a9fb3a1d6f5d82381067e4774ffa
Michel Bitter <michelbitter@live.de>
2023-09-18 17:45:11 +0200

    Sanctum Url korrigieren

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,28 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { HttpResponse, HttpHeaders } from '@angular/common/http';
-import { BACKEND_API_URL, BACKEND_URL } from './constants';
-import { AppResult } from './shared/interfaces/AppResult';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { HttpResponse, HttpHeaders } from "@angular/common/http";
+import { BACKEND_API_URL, BACKEND_URL } from "./constants";
+import { AppResult } from "./shared/interfaces/AppResult";
 
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: "root"
 })
 export class AuthenticationService {
-	authUrl = BACKEND_URL + "/sanctum/csrf-cookie";
+	authUrl = "/sanctum/csrf-cookie";
 	loginUrl = BACKEND_API_URL + "/login";
 
 	constructor(private http: HttpClient) { }
 
 	// Authentifiziert App ggü. Backend
 	public authenticateApp(): Observable<HttpResponse<Object>> {
-		return this.http.get(this.authUrl, { observe: 'response', withCredentials: true });
+		return this.http.get(this.authUrl, { observe: "response", withCredentials: true });
 	}
 
 	// User einloggen
 	public login(credentialObject: Object): Observable<HttpResponse<AppResult>> {
-		return this.http.post<AppResult>(this.loginUrl, credentialObject, { headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*' }), observe: 'response', withCredentials: true });
+		return this.http.post<AppResult>(this.loginUrl, credentialObject, { headers: new HttpHeaders({ "Access-Control-Allow-Origin": "*" }), observe: "response", withCredentials: true });
 	}
 
 	// Loggt User aus
